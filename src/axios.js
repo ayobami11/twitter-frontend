428b8a43e54fbd3b79b264c84f5998126fe0bc4f
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2023-08-15 20:48:10 +0100

    Updated react-scripts library

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const apiClient = () => {
     const defaultOptions = {
-        // baseURL: 'https://tweetteer-backend.herokuapp.com/api/v1',
         baseURL: 'https://twitter-backend-poqg.onrender.com/api/v1',
         headers: {
             'Content-Type': 'application/json'
